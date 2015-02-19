ef6e61b24d77d5425838b76ddf3fde015d338578
julianeb <julianeb@reallyenglish.com>
2015-02-19 10:29:56 +0000

    Random changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,11 +3,28 @@
 $(document).ready(function() {
   $.getJSON('/data/words.json', function(words) {
     $.each(words, function(i, entry) {
+
       var container = $('<div>');
 
       container.text(entry.word);
-
+      container.addClass('word');
+      container.attr("onclick", "changeImage" + entry.word + "()")
+      container.attr("data-ref", entry.word);
       $('body').append(container);
     });
+
+    $(".word").hover(function() {
+      $(this).css("background-color", "#993399");
+      var imageId = "#" + $(this).attr('data-ref');
+      $(imageId).animate({'width':'100%', 'height':'100%'});
+    }, 
+    function() {
+      $(this).css("background-color", "#FF5050");
+      var imageId = "#" + $(this).attr('data-ref');
+      $(imageId).animate({'width':'50%', 'height':'50%'});    
+
+    });
+
   });
-});
+
+});  
